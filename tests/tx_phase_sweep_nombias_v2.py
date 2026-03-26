83a2b25914d4f26b82ae63f7feb51cb3ff3d7368
Silicon Validation <silicon.validation@fermionic.design>
2026-03-26 19:00:13 +0530

    Update tx_phase_sweep_nombias_v2.py

--- tests/tx_phase_sweep_nombias_v2.py
+++ tests/tx_phase_sweep_nombias_v2.py
@@ -1,7 +1,7 @@
 version = 'v2'
 ant_sel = 0x1
 chip_id = 'AE123'
-test_condition = 'max_bias_at_inp_p1dB_16dB_attn'
+test_condition = 'max_bias_at_inp_p1dB_0dB_attn'
 
 f = 14.25
 f_min = 10.0
@@ -65,9 +65,7 @@ orion_lut = ORION_8G_12G_lut(spi)
 orion_hal = ORION_8G_12G_hal(orion_csr,orion_lut,spi,version)
 
 orion_hal.init_lut_new(r'C:/Users/silic/OneDrive/Documents/GitHub/orion/final_lut/TX_Gain_LUT_10p5GHz.xlsx',
-                       r'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/LUT/tx_v2__phase_lut_freq_14p25_gm_0p5_pm_1p5_pm2_4_abs_gain_8__maxbias__vdd_2p7.xlsx',
-                       # r'C:/Users/silic/OneDrive/Documents/GitHub/orion/final_lut/v2__tx_phase_lut__freq_8p5__gm_0p5__pm_1p5__pm2_3__abs_gain_7p5__nom__vdd_2p7.xlsx',
-                       # r'C:/Users/silic/OneDrive/Documents/GitHub/orion/final_lut/v2__tx_phase_lut__freq_9p5__gm_0p5__pm_1p5__pm2_3__abs_gain_6p5__low__vdd_2p5.xlsx',
+                       r'C:/Users/silic/OneDrive/Documents/GitHub/orion/final_lut/tx_v2__phase_lut_freq_14p25_gm_0p5_pm_1p5_pm2_4_abs_gain_8__maxbias__vdd_2p7.xlsx',
                        r'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/LUT/v2__rx_gain_lut__nom_bias__9p5GHz.xlsx',
                        r'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/LUT/v2__phase_lut_freq_9p5_gm_1_pm_1p4.xlsx',
                        r'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/LUT/v2__rx_gain_lut__nom_bias__9p5GHz.xlsx',
@@ -81,7 +79,7 @@ orion_hal.cfg_stg2_load('REG')
 orion_hal.en_data_path(1)
 
 # Normalize
-orion_hal.set_lut_idx(4,53,ant_sel)
+orion_hal.set_lut_idx(4,0,ant_sel)
 orion_hal.stg2_load()
 time.sleep(d1)
 instruments.vna.write(":SENS:AVER:CLE")          # Clear averaging
@@ -99,7 +97,7 @@ time.sleep(d2)
 #
 # exit()
 # Initialize xlsx for read
-filename = f'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/bench_char/tx_char__{version}__{chip_id}__ant_sel_{ant_sel}__{test_condition}__{ts}.xlsx'
+filename = f'C:/Users/silic/OneDrive/Documents/GitHub/orion/results/tx_char__{version}__{chip_id}__ant_sel_{ant_sel}__{test_condition}__{ts}.xlsx'
 out_xls = xlsw.Workbook(filename)
 
 out_sheet = out_xls.add_worksheet()
@@ -131,7 +129,7 @@ for i in range(0,freq_pts,1):
 
 xls_row=1
 for p_idx in range (4,125,1):
-    orion_hal.set_lut_idx(p_idx,53,ant_sel)
+    orion_hal.set_lut_idx(p_idx,0,ant_sel)
     orion_hal.stg2_load()
     time.sleep(d1)
     instruments.vna.write(":SENS:AVER:CLE")
